cd683a87820ff51c14759efc2b1459b71851ec98
Nicolas <nicolasperaltait@gmail.com>
2024-08-25 23:11:08 -0300

    .

--- 00-Base.sh
+++ 00-Base.sh
@@ -12,21 +12,21 @@ apt install ufw clamav clamav-daemon git wget curl zsh htop preload -y &&
 # Firewall setup
 
 # Puertos Especificos
-    sudo ufw limit 22/tcp                                                 # Limita las coneccions por puerto 22 ssh 
+    sudo ufw limit 22/tcp                                               # Limita las coneccions por puerto 22 ssh 
 
   # Reglas Generales
     sudo ufw default deny incoming
     sudo ufw default allow outgoing
 
   #habilitacion
-    sudo ufw enable                                                       # Habilitar UFW para gestionar el firewall
+    sudo ufw enable                                                     # Habilitar UFW para gestionar el firewall
 
 # AV setup
 
-    sudo systemctl stop clamav-freshclam                                   # Detener el servicio de actualización de ClamAV
-    sudo freshclam                                                         # Actualizar la base de datos de firmas de virus de ClamAV
-    sudo systemctl start clamav-freshclam                                  # Iniciar de nuevo el servicio de actualización de ClamAV
-    sudo systemctl start clamav-daemon                                     # Iniciar de nuevo el daemon de actualización de ClamAV
+    sudo systemctl stop clamav-freshclam                                # Detener el servicio de actualización de ClamAV
+    sudo freshclam                                                      # Actualizar la base de datos de firmas de virus de ClamAV
+    sudo systemctl start clamav-freshclam                               # Iniciar de nuevo el servicio de actualización de ClamAV
+    sudo systemctl start clamav-daemon                                  # Iniciar de nuevo el daemon de actualización de ClamAV
 
 # ZSH + OhMyZsh
 
