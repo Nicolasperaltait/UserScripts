2174dd338baa16b9d5ec0776e1930ec5ee4230cd
Nicolas <nicolasperaltait@gmail.com>
2025-01-21 13:01:30 -0300

    add pwlvl10k

--- Zsh.sh
+++ Zsh.sh
@@ -6,3 +6,11 @@ sudo apt install zsh curl -y
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+
+
+#Opcional 
+
+
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# nano .zshrc
