fe60a423cde7f888f153d208681dd7649f28d85e
Nicolas <nicolasperaltait@gmail.com>
2025-01-21 14:46:11 -0300

    .

--- 00-Base.sh
+++ 00-Base.sh
@@ -10,7 +10,7 @@ sudo usermod -a -G sudo nicolas
 #Basics Instalation
 apt install ufw clamav clamav-daemon git wget curl zsh htop preload nala neofetch -y &&
 
-# Firewall setup
+# Firewall setups
 
 # Puertos Especificos
     sudo ufw limit 22/tcp  # Limita las coneccions por puerto 22 ssh
