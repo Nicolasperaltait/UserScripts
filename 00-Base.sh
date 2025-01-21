a13d16f567e9af711f89180901b792e6e85b8b08
Nicolas <nicolasperaltait@gmail.com>
2025-01-21 11:23:54 -0300

    add proxmox port

--- 00-Base.sh
+++ 00-Base.sh
@@ -13,7 +13,8 @@ apt install ufw clamav clamav-daemon git wget curl zsh htop preload -y &&
 # Firewall setup
 
 # Puertos Especificos
-    sudo ufw limit 22/tcp                                               # Limita las coneccions por puerto 22 ssh 
+    sudo ufw limit 22/tcp  # Limita las coneccions por puerto 22 ssh
+    sudo ufw allow 8006    # Permite el acceso al puerto 8006 usado por proxmox                                          
     
   # Reglas Generales
     sudo ufw default deny incoming
