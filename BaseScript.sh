2a8f6cda08a2d3fb984983ab49991adc94d06420
Nicolas <nicolasperaltait@gmail.com>
2025-01-26 00:15:24 -0300

    changes

--- BaseScript.sh
+++ BaseScript.sh
@@ -21,6 +21,8 @@ apt install ufw clamav clamav-daemon git wget curl zsh htop preload nala neofetc
 # Puertos Especificos
     sudo ufw limit 22/tcp  # Limita las coneccions por puerto 22 ssh
     sudo ufw allow 8006    # Permite el acceso al puerto 8006 usado por proxmox                                          
+    sudo ufw allow 3306    # Permite el acceso al puerto 3306 usado por MySQL
+    sudo ufw allow 3389    # Permite el acceso al puerto 3389 usado por RDP
     
   # Reglas Generales
     sudo ufw default deny incoming
