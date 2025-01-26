b7709204b0f93fbb90dd3c5989ce4128b2711f05
Nicolas <nicolasperaltait@gmail.com>
2025-01-26 01:39:43 -0300

    .

--- Install.sh
+++ Install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-apt update && apt upgrade -y 
-
-apt install sudo git -y 
+apt update && apt upgrade -y && apt install sudo git -y 
 
 mkdir -p github && cd github
 
 git clone https://github.com/Nicolasperaltait/UserScripts.git
 
-cd UserScripts && chmod 775 *.sh && ./00-base.sh
+cd UserScripts && chmod 775 *.sh 
 
+install.sh
--- Single Scripts/PowerLvl10k.sh
+++ Single Scripts/PowerLvl10k.sh
@@ -2,7 +2,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 
 # ZSH_THEME="robbyrussell"
-#ZSH_THEME="powerlevel10k/powerlevel10k "
+#ZSH_THEME="powerlevel10k/powerlevel10k"
 # agregar powerlevel10k/powerlevel10k 
 
 # original robbyrussell
