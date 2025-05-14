adb86ddcd885a1511a0277ae3489d00d441d8fd5
Nicolas Peralta <nicolasperaltait@gmail.com>
2025-05-13 23:10:50 -0300

    add auto paste

--- BaseScript.sh
+++ BaseScript.sh
@@ -54,10 +54,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 cd / && cd root && nano .zshrc
 
 
-#add to the .zhhrc in plugins part whitout #
-
-
-            #   plugins=(git
-            #   sudo
-            #   zsh-syntax-highlighting
-            #   zsh-autosuggestions)
+# Verifica si la línea plugins=(git está sola en el .zshrc de root y la reemplaza por la lista completa de plugins
+ZSHRC_PATH="/root/.zshrc"
+if grep -q '^plugins=(git)$' "$ZSHRC_PATH"; then
+    # Reemplaza la línea por la lista completa de plugins
+    sed -i 's/^plugins=(git)$/plugins=(git sudo zsh-syntax-highlighting zsh-autosuggestions)/' "$ZSHRC_PATH"
+fi
