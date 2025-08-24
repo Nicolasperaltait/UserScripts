7f6574942239edd354c63ad2704d6a1f99f6dc23
Nicolas Peralta <nicolasperaltait@gmail.com>
2025-08-24 20:21:00 -0300

    x

--- BaseScript.sh
+++ BaseScript.sh
@@ -37,3 +37,35 @@ apt install ufw clamav clamav-daemon git wget curl zsh htop preload nala neofetc
     sudo freshclam                                                      # Actualizar la base de datos de firmas de virus de ClamAV
     sudo systemctl start clamav-freshclam                               # Iniciar de nuevo el servicio de actualización de ClamAV
     sudo systemctl start clamav-daemon                                  # Iniciar de nuevo el daemon de actualización de ClamAV
+
+# ZSH + OhMyZsh
+
+# Pedir el nombre de usuario para la instalación
+read -p "Ingrese el nombre de usuario en el cual desea instalar Oh My Zsh: " TARGET_USER
+
+# Verificar que el usuario existe
+if ! id "$TARGET_USER" &>/dev/null; then
+    echo "El usuario '$TARGET_USER' no existe. Abortando."
+    exit 1
+fi
+
+# No permitir instalar en root
+if [ "$TARGET_USER" = "root" ]; then
+    echo "No se recomienda instalar Oh My Zsh en root. Por favor, elija un usuario normal."
+    exit 1
+fi
+
+TARGET_HOME=$(eval echo "~$TARGET_USER")
+
+# Instalar Oh My Zsh para el usuario especificado
+sudo -u "$TARGET_USER" sh -c "echo 'y' | sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
+
+# Cambiar la shell por defecto a zsh para el usuario especificado
+sudo chsh -s "$(which zsh)" "$TARGET_USER"
+
+# Instalar plugins para el usuario especificado
+sudo -u "$TARGET_USER" git clone https://github.com/zsh-users/zsh-autosuggestions "${TARGET_HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+sudo -u "$TARGET_USER" git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${TARGET_HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+
+# Abrir el .zshrc del usuario para editar
+sudo -u "$TARGET_USER" nano "${TARGET_HOME}/.zshrc"
--- Single Scripts/Zsh.sh
+++ Single Scripts/Zsh.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-# ZSH + OhMyZsh
 
-echo "y" | sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt update && sudo apt upgrade -y
 
-# Run To make ZSH default shell
+sudo apt install zsh curl -y
 
-chsh -s $(which zsh) &&
-
-# Plugins Zsh
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
-
-# cd / && cd root && nano .zshrc
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
-# Verifica si la línea plugins=(git está sola en el .zshrc de root y la reemplaza por la lista completa de plugins
-ZSHRC_PATH="/root/.zshrc"
-if grep -q '^plugins=(git)$' "$ZSHRC_PATH"; then
-    # Reemplaza la línea por la lista completa de plugins
-    sed -i 's/^plugins=(git)$/plugins=(git sudo zsh-syntax-highlighting zsh-autosuggestions)/' "$ZSHRC_PATH"
-fi
+
+
+#Opcional 
+
+
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# nano .zshrc
